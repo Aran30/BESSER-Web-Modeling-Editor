f12a02e7d84921a573747fff52227f00cd3f191f
ArmSL <scorp-arm@hotmail.com>
2025-02-20 17:23:29 +0100

    Add StateMachineDiagram type and UMLState interface for state modeling

--- docs/source/user/api/typings.d.ts
+++ docs/source/user/api/typings.d.ts
@@ -87,6 +87,14 @@ export type UMLClassifier = UMLElement & {
     attributes: string[];
     methods: string[];
 };
+export interface UMLState extends UMLElement {
+    type: UMLElementType;
+    bodies: string[];
+    fallbackBodies: string[];
+}
+export type UMLStateTransition = UMLRelationship & {
+    params?: string | string[];
+};
 export type UMLDeploymentNode = UMLElement & {
     stereotype: string;
     displayStereotype: boolean;
